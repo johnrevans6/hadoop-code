ef13e06559ca1f791c236e390a159b842027b7f8
johnrevans6 <johnre@hawaii.edu>
2017-02-20 17:22:30 -0800

    Inital Commit

---
+++ HadoopMapReduce/src/main/java/WordCountMR/HadoopMapReduce/WordCountMapper.java
@@ -0,0 +1,29 @@
+package WordCountMR.HadoopMapReduce;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WordCountMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+ private final static IntWritable one = new IntWritable(1);
+ private Text word = new Text();
+
+ public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> collector, Reporter reporter)
+   throws IOException {
+  String line = value.toString();
+  StringTokenizer st = new StringTokenizer(line, " ");
+  while (st.hasMoreTokens()) {
+   word.set(st.nextToken());
+   collector.collect(word, one);
+  }
+
+ }
+}
